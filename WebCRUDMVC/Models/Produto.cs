c398f9e5a34e08e07f46d6cd15b0e3c6ef8059ed
Markus Lima <markusmak@gmail.com>
2023-03-13 11:10:06 -0300

    first commit

---
+++ WebCRUDMVC/Models/Produto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebCRUDMVC.Models
+{
+    [Table("Produtos")]
+    public class Produto
+    {
+        [Column("Id")]
+        [Display(Name = "Codigo")]
+        public int Id { get; set; }
+
+        [Column("Nome")]
+        [Display(Name = "Nome")]
+        public string Nome { get; set; }
+
+    }
+}
